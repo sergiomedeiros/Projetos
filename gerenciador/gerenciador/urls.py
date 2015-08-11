adda3dea62095978baac2af832643684e2d2b9ef
sergiomedeiros <sergio_pergunta@hotmail.com>
2015-08-11 20:51:21 -0300

    teste 2

--- gerenciador/gerenciador/urls.py
+++ gerenciador/gerenciador/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from gerenciador.core.views import home
+
 urlpatterns = [
+    url(r'^$',home,name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
